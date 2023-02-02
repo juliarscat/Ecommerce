a47599df1db63beb0e8287eeb7f593f7c5eb03b9
juliarscat <123875621+juliarscat@users.noreply.github.com>
2023-02-02 14:05:31 +0100

    Create bizumpayment.php

---
+++ opencart/bizum/catalog/language/english/payment/bizumpayment.php
@@ -0,0 +1,6 @@
+<?php
+$_['text_title'] = 'Bizum Payment';
+$_['text_instruction'] = 'Bizum Payment Instructions:';
+$_['text_description'] = 'Please transfer the total amount to the following Bizum number.';
+$_['text_payment'] = 'Your order will not be processed until we receive payment.';
+$_['text_bizum_number'] = 'Bizum Number:';
